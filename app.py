f6cc81dba33b025b74e3d76f2e921dd4cbad9018
Abdullah <abdullahwazid31006@gmail.com>
2026-07-03 00:59:47 +0530

    Conversion Logic and API Integration

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import json
+import requests
 import streamlit as st 
 
 st.title("Currency Converter")
@@ -7,7 +8,18 @@ st.title("Currency Converter")
 with open("currencies.json", "r", encoding="utf-8") as file : 
     currencies = json.load(file)
 
-from_currency = st.selectbox(
+#defining function for swap
+def swap_currencies() : 
+    st.session_state.from_currency, st.session_state.to_currency = (
+            st.session_state.to_currency,
+            st.session_state.from_currency,
+        )
+
+#adding colummns for from swap and to currency
+col1, col2, col3 = st.columns([4,1,4])
+
+with col1 : 
+    from_currency = st.selectbox(
     "From",
     currencies,
     key = "from_currency",
@@ -15,10 +27,16 @@ from_currency = st.selectbox(
         f'{currency["flag"]} {currency["code"]} - {currency["name"]}'
 )
 
-to_currency = st.selectbox(
+with col2 : 
+    st.write(" ") #spacing
+    st.write(" ")
+    st.button("🔄", on_click=swap_currencies)
+
+with col3 : 
+    to_currency = st.selectbox(
     "To",
     currencies,
-    key = "from_currency",
+    key = "to_currency",
     format_func=lambda currency : 
         f'{currency["flag"]} {currency["code"]} - {currency["name"]}'
 )
@@ -30,4 +48,16 @@ from_amount = st.number_input(
     step = 0.10,
 )
 
+#api & conversion
+if st.button("Convert") : 
+    url = f"https://open.er-api.com/v6/latest/{from_currency['code']}"
+    response = requests.get(url)
+    data = response.json()
 
+    rate = data["rates"][to_currency["code"]]
+    converted_amount = from_amount * rate
+    
+    st.success(
+        f"{from_amount:.2f} {from_currency['code']} = "
+        f"{converted_amount:.6f} {to_currency['code']}"
+    )
